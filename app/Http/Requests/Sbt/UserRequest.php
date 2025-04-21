ceae4059ca9d40a40e1d8189e526080e1c9c982d
mukhsamr <mukhsamr@gmail.com>
2025-04-21 09:15:53 +0700

    fix: user request

---
+++ app/Http/Requests/Sbt/UserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Sbt;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'username' => 'required|unique:App\Models\Sbt\User,username,' . $this->user?->id,
+            'password' => 'sometimes|required|min:8',
+            'role' => 'sometimes|required|in:admin,operator,student',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'username.required' => 'Username wajib diisi.',
+            'username.unique' => 'Username sudah digunakan.',
+            'password.required' => 'Password wajib diisi.',
+            'password.min' => 'Password harus terdiri dari 8 karakter atau lebih.',
+            'role.required' => 'Role wajib diisi.',
+            'role.in' => 'Role tidak valid.',
+        ];
+    }
+}
