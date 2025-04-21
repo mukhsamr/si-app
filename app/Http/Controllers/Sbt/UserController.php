89743e92b0284fe8c3f21f3acd38718cb077f04c
mukhsamr <mukhsamr@gmail.com>
2025-04-21 09:18:38 +0700

    fix: sbt user request

---
+++ app/Http/Controllers/Sbt/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Sdt;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Sbt\UserRequest;
+use App\Http\Resources\Sbt\UserResource;
+use App\Models\Sbt\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserController extends Controller
+{
+    function update(UserRequest $request, User $user): JsonResource
+    {
+        $user->update($request->validated());
+        return UserResource::make($user)
+            ->additional([
+                'message' => 'User updated successfully',
+                'status' => 'success'
+            ]);
+    }
+}
